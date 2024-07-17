2aa860f9f8e3dbf3e827b9bba0db11ae242c8dc6
sahalhakami <147971142+sahalhakami@users.noreply.github.com>
2024-07-17 17:33:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+function go (params) {
+    
+    if (select1.value == "datatype" && radio1.checked ==  true) {
+        location.href= "data type.html";
+    }
+     else if (select1.value == "mo3amlat" && radio1.checked ==  true){
+        location.href= "mo3amlat python.html";
+     }
+     else if (select1.value == "math" && radio1.checked ==  true){
+        location.href= "math mo3amlat.html";
+     }
+     else if (select1.value == "esnad" && radio1.checked ==  true){
+        location.href= "esnad mo3amlat.html";
+     }
+     else if (select1.value == "draw" && radio1.checked ==  true){
+        location.href= "draw.html";
+     }
+     else if (select1.value == "funcationd" && radio1.checked ==  true){
+        location.href= "drawfunction.html";
+     }
+     else if (select1.value == "turtle" && radio1.checked ==  true){
+        location.href= "turtle shape.html";
+     }
+     else if (select1.value == "line" && radio1.checked ==  true){
+        location.href= "draw line.html";
+     }
+     else if (select1.value == "qurtare" && radio1.checked ==  true){
+        location.href= "draw qurtare.html";
+     }
+     else if (select1.value == "circle" && radio1.checked ==  true){
+        location.href= "draw circle.html";
+     }
+     else if (select1.value == "tringle" && radio1.checked ==  true){
+        location.href= "draw tringle.html";
+     }
+     else if (select1.value == "write" && radio1.checked ==  true){
+        location.href= "write text.html";
+     }
+     else if (select1.value == "shapes" && radio1.checked ==  true){
+        location.href= "draw shape.html";
+     }
+     else if (select1.value == "quiz"){
+      location.href= "quiz.html";
+   }
+     else if (select1.value == "quiz" && radio1.checked ==  true){
+      location.href= "quiz.html";
+   }
+   
+     
+
+   else  if (select1.value == "datatype" && radio2.checked ==  true) {
+      location.href= "data type video.html";
+  }
+   else if (select1.value == "mo3amlat" && radio2.checked ==  true){
+      location.href= "mo3amlat python video.html";
+   }
+   else if (select1.value == "math" && radio2.checked ==  true){
+      location.href= "math mo3amlat video.html";
+   }
+   else if (select1.value == "esnad" && radio2.checked ==  true){
+      location.href= "esnad mo3amlat video.html";
+   }
+   else if (select1.value == "draw" && radio2.checked ==  true){
+      location.href= "draw video.html";
+   }
+   else if (select1.value == "funcationd" && radio2.checked ==  true){
+      location.href= "drawfunction video.html";
+   }
+   else if (select1.value == "turtle" && radio2.checked ==  true){
+      location.href= "turtle shape video.html";
+   }
+   else if (select1.value == "line" && radio2.checked ==  true){
+      location.href= "draw line video.html";
+   }
+   else if (select1.value == "qurtare" && radio2.checked ==  true){
+      location.href= "draw qurtarevideo.html";
+   }
+   else if (select1.value == "circle" && radio2.checked ==  true){
+      location.href= "draw circle video.html";
+   }
+   else if (select1.value == "tringle" && radio2.checked ==  true){
+      location.href= "draw tringle video.html";
+   }
+   else if (select1.value == "write" && radio2.checked ==  true){
+      location.href= "write text video.html";
+   }
+   else if (select1.value == "shapes" && radio2.checked ==  true){
+      location.href= "draw shape video.html";
+   }
+   else if (select1.value == "quiz" && radio2.checked ==  true){
+      location.href= "quiz.html";
+   }
+     
+    
+}
+
+
+function goit(params){
+   if (txta.value ==4444) {
+      location.href= "home.html";
+   }
+
+}
